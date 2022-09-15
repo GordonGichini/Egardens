f2c5ed2d5c3f346c3386d1b931aa82cf9fed95cb
GordonGichini <gordongichini1@gmail.com>
2022-09-15 04:12:43 +0300

    modified component

--- my-app-frontend/src/Product.jsx
+++ my-app-frontend/src/Product.jsx
@@ -19,6 +19,26 @@ export default class Product extends Component {
 
                 <div>${this.state.product.price}</div>
         </div>
+        {/* card body ends here*/}
+
+        <div className="card-footer">
+          <div className="float-left">
+            <span className="badge">{this.state.product.quantity}</span>
+
+            <div className="btn-group">
+                <button className="btn btn-outline-success"onClick={() => {
+                    this.props.onIncrement(this.state.product,10);
+                    }}>+</button>
+                <button className="btn btn-outline-success"onClick={() => {
+                    this.props.onDecrement(this.state.product,0);
+                    }}>-</button>
+            </div>
+            </div>  
+            {/*float-left ends here */}
+
+             <div className="float-right">{this.props.children}</div>
+        </div>
+        {/*card-footer ends here */}
         </div>
         </div>
         );
--- my-app-frontend/src/ShoppingCart.jsx
+++ my-app-frontend/src/ShoppingCart.jsx
@@ -19,10 +19,42 @@ export default class ShoppingCart extends Component{
 
             <div className="row">
                 {this.state.products.map((prod)=> {
-                    return <Product key={prod.id}product={prod}/>;
+                    return <Product 
+                    key={prod.id}
+                    product={prod}
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                    >
+                    <button className="btn btn-primary">Buy Now</button></Product>;
                 })}
             </div>
         </div>
         );
     }
-}
+    // render ends here
+
+    //executes when the user clicks on + button
+    handleIncrement=(product,maxValue) => {
+        
+        //get index of the selected product
+        let allProducts=[...this.state.products];
+        let index=allProducts.indexOf(product);
+        
+        if(allProducts[index].quantity<maxValue){
+            allProducts[index].quantity++;
+//update the state of the current component(parent component)
+        this.setState({products:allProducts});
+        }
+    };
+
+    handleDecrement=(product,minValue) => {
+         //get index of the selected product
+         let allProducts=[...this.state.products];
+         let index=allProducts.indexOf(product);
+         
+         if(allProducts[index].quantity>minValue)
+         allProducts[index].quantity--;
+ //update the state of the current component(parent component)
+         this.setState({products:allProducts});
+     };
+    };
