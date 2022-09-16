bd2ef4e78e6d4e7b280678106d88be80ab8fcfcc
GordonGichini <gordongichini1@gmail.com>
2022-09-16 11:35:17 +0300

    404 page added

--- my-app-frontend/src/App.jsx
+++ my-app-frontend/src/App.jsx
@@ -4,18 +4,22 @@ import Dashboard from "./Dashboard";
 import ShoppingCart from "./ShoppingCart";
 import CustomerList from "./CustomerList";
 import Login from "./Login";
-import {Route} from "react-router";
+import {Route, Switch} from "react-router";
 import {BrowserRouter} from "react-router-dom";
+import NoMatchPage from "./NoMatchPage"
 
 export default class App extends Component{
 render(){
 return( 
 <BrowserRouter>
     <NavBar />
+    <Switch>
     <Route path="/"exact component={Login}/>
     <Route path="/dashboard"exact component={Dashboard}/>
     <Route path="/customers"exact component={CustomerList}/>
     <Route path="/cart"exact component={ShoppingCart}/>
+    <Route path="*" component={NoMatchPage}/>
+    </Switch>
 </BrowserRouter>
 );
 }
