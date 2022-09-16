ce0cdcc168eaf1924a591403f1c1f0a4245788f7
GordonGichini <gordongichini1@gmail.com>
2022-09-16 11:18:30 +0300

    added dashboard

---
+++ my-app-frontend/src/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import { render } from "react-dom";
+
+export default class Dashboard extends Component{
+    render(){
+        return(
+            <div>
+                <h4>Dashboard</h4>
+            </div>
+        );
+    }
+}
